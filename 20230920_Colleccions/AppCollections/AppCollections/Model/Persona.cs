0c40c8b4c02ade54b801d8a0f2688cae37266070
Bernat <bernat.orellana@gmail.com>
2023-09-20 20:32:09 +0200

    Proves amb llistes

---
+++ 20230920_Colleccions/AppCollections/AppCollections/Model/Persona.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCollections.Model
+{
+    internal class Persona
+    {
+        private long id;
+        private string name;
+        private int age;
+
+        public Persona(long id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+
+        public long Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+
+        public override string ToString()
+        {
+            return $"ID:{Id}, Name:{Name}, Age:{Age}";
+        }
+    }
+}
