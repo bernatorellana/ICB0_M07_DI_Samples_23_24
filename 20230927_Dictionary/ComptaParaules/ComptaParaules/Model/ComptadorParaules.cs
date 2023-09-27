99445696b9d05fcf45f38a6b14df1a56a8d19c58
Bernat <bernat.orellana@gmail.com>
2023-09-27 19:55:16 +0200

    lectura del llibre Dracula

---
+++ 20230927_Dictionary/ComptaParaules/ComptaParaules/Model/ComptadorParaules.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace ComptaParaules.Model
+{
+    internal class ComptadorParaules
+    {
+        public async Task carregaArxiuAsync(String arxiu)
+        {
+            var file = await StorageFile.GetFileFromApplicationUriAsync(
+                new Uri($"ms-appx:///Assets/{arxiu}"));
+            using (var inputStream = await file.OpenReadAsync())
+            using (var classicStream = inputStream.AsStreamForRead())
+            using (var streamReader = new StreamReader(classicStream))
+            {
+                while (streamReader.Peek() >= 0)
+                {
+                    Debug.WriteLine(string.Format("the line is {0}",
+                        streamReader.ReadLine()));
+                }
+            }
+        }
+    }
+}
