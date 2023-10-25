01155032a86f7b63c8d3f3cb386d0acb781d15ce
developrepogithub <develop.repo.github@gmail.com>
2023-10-25 21:27:08 +0200

    NumericTexbox

---
+++ 20231025_UC_NT/20231025_UC_NT/20231025_UC_NT/View/UINumericTextbox.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Control de usuario está documentada en https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace _20231025_UC_NT.View
+{
+    public sealed partial class UINumericTextbox : UserControl
+    {
+        public UINumericTextbox()
+        {
+            this.InitializeComponent();
+        }
+        public event EventHandler<EventArgs> ValueChanged;
+
+        #region Prop-dps
+
+
+        public int Value
+        {
+            get { return (int)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Value.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(int), typeof(UINumericTextbox), new PropertyMetadata(0));
+
+
+        public int Max
+        {
+            get { return (int)GetValue(MaxProperty); }
+            set { SetValue(MaxProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Max.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MaxProperty =
+            DependencyProperty.Register("Max", typeof(int), typeof(UINumericTextbox), new PropertyMetadata(100));
+
+
+        public int Min
+        {
+            get { return (int)GetValue(MinProperty); }
+            set { SetValue(MinProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Min.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MinProperty =
+            DependencyProperty.Register("Min", typeof(int), typeof(UINumericTextbox), new PropertyMetadata(0));
+
+
+        #endregion
+
+        private void btnUp_Click(object sender, RoutedEventArgs e)
+        {
+            Value++;
+        }
+
+        private void btnDown_Click(object sender, RoutedEventArgs e)
+        {
+            Value--;
+        }
+
+        private void txtValue_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int value;
+            bool isNumber = Int32.TryParse(txtValue.Text, out value);
+            if(isNumber)
+            {
+                Value = value;
+            } else
+            {
+                Value = 0;
+            }
+            
+        }
+
+        private void txtValue_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if ( !(
+                e.Key==VirtualKey.Back ||e.Key==VirtualKey.Delete||
+                e.Key==VirtualKey.Left || e.Key == VirtualKey.Right ||
+                (e.Key>=Windows.System.VirtualKey.Number0 && e.Key <= Windows.System.VirtualKey.Number9) ||
+                (e.Key>=Windows.System.VirtualKey.NumberPad0 && e.Key <= Windows.System.VirtualKey.NumberPad9)
+                )
+            )
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtValue_ContextMenuOpening(object sender, ContextMenuEventArgs e)
+        {
+            e.Handled= true;
+        }
+    }
+}
